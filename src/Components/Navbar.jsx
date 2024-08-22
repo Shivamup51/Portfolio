1997e8be2a502e0282b4a5f3e05e8735665e5079
shivam upadhyay <shivamupadhyay2868@gmail.com>
2024-08-22 14:25:39 +0530

    first commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/shivamlogo.png";
+import logo from "../assets/kevinRushLogo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaMailBulk } from "react-icons/fa";
@@ -8,11 +8,7 @@ const Navbar = () => {
     return (
         <nav className="mb-20 flex items-center justify-between py-10">
             <div className="flex flex-shrink-0 items-center">
-                <img
-                    className="mx-6 w-12  rounded-xl border-neutral-900"
-                    src={logo}
-                    alt="logo"
-                />
+                
             </div>
 
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
