1d2b6e6e24eb34b6757e6dc987487ede9076e1df
Shivam Upadhyay <166123727+Shivamup51@users.noreply.github.com>
2024-10-17 13:51:54 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,7 @@ import project3 from "../assets/projects/Project-3.png";
 import project4 from "../assets/projects/Project-4.png";
 
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 1-year of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 1-year of hands-on experience, I have honed my skills in front-end technologies like React and TailwindCss, as well as back-end technologies like Node.js, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a  full stack developer with a passion for creating efficient and user-friendly web applications.I have worked with a variety of technologies, including Html, Css, tailwindcss, React, Node.js, Express.j, MySQL,and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
